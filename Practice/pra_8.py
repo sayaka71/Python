755b1d90d670d1f7048b4490ec6f63ea276141b4
brunk <saya.56k@gmail.com>
2021-07-10 17:52:04 +0900

    PATH (モジュールのパス)

---
+++ Practice/pra_8.py
@@ -0,0 +1,13 @@
+# Python入門
+# モジュールのパスを通す(p.266)ea
+
+# sysをインポート
+import sys
+
+# 検索パスを追加している
+sys.path.append('/Users/brunk/df_python/functions/')
+
+# 通したパスからインポート
+import pdf
+
+# setup.pyファイルを作成するとめちゃくちゃ便利
