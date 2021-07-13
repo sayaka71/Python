e4a6b7311fe230cd899e52f97c76084c63884730
brunk <saya.56k@gmail.com>
2021-07-13 10:47:02 +0900

    Algorithm

---
+++ Algorithm/alg_01.py
@@ -0,0 +1,23 @@
+# Intoroduction
+
+"""
+bitとbyte:
+    0 or 1 しかコンピュータは扱えない。
+    1 bit = 2^1
+    8 bit = 2^8 = 256 パターン
+    8 bit = 1 byte
+"""
+
+
+a = 3 # 整数(integer)
+print(type(a))
+
+b = 1.5 # 実数(float)
+print(type(b))
+
+name1 = "あい"
+name2 = "うえお"
+
+name3 = name1 + name2
+print(type(name3))
+print(name3)
