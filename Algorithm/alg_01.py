3dfd9edd4e5c277a5d38bf668908ad3e637b8872
brunk <saya.56k@gmail.com>
2021-07-13 10:56:33 +0900

    型

--- Algorithm/alg_01.py
+++ Algorithm/alg_01.py
@@ -8,7 +8,6 @@ bitとbyte:
     8 bit = 1 byte
 """
 
-
 a = 3 # 整数(integer)
 print(type(a))
 
@@ -17,7 +16,15 @@ print(type(b))
 
 name1 = "あい"
 name2 = "うえお"
-
 name3 = name1 + name2
+age = 22
+
 print(type(name3))
 print(name3)
+
+# 練習問題
+# name1, name2, ageを使って
+# '私はあいうえおです。22歳です。'
+# と表示するプログラムを作成。
+print('私は{}です。{}です。'.format(name3, age))
+print('私は' + name3 + 'です。' + str(age) + 'です。') #ageを文字列に直して型を整える
