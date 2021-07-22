2f4d0c95e71b29ed342b60d2aef4df78ae12fc32
brunk <saya.56k@gmail.com>
2021-07-22 10:54:36 +0900

    my answer

--- Algorithm/alg_02.py
+++ Algorithm/alg_02.py
@@ -58,14 +58,70 @@ add_lists(test_list1, test_list3)
 # 圧縮のアルゴリズム
 with open("input.txt") as f:
     text = f.read()
-print(len(text))
+
 # originalのファイルを情報が劣化しないように圧縮する
 # 圧縮を行う関数：text_encode
 # 復元を行う関数：text_decode
 
-# 10001 = 1[01] 0[03] 1[01]
-text100 = text[:100]
+raw_text = text[:300]
+print(f'raw text: {raw_text}\n')
 
-for _text in text100:
-    if _text == "1":
-        num = _text
+# 圧縮を行う関数：text_encode
+def text_encode(text):
+    """
+    10001 = 1[01] 0[03] 1[01]という法則でエンコードする
+    myfile.txtに書き込んで圧縮ファイル作成
+    """
+    f = open('encode.txt', 'w')
+    # first
+    first_text = text[0]
+    f.write(first_text)
+    cnt = 1
+
+    for _text in text[1:]:
+        if _text == first_text:
+            num = _text
+            cnt += 1
+
+        elif _text != first_text:
+            if cnt < 10:
+                cnt = '0' + str(cnt)
+            f.write(str(cnt))
+            first_text = _text
+            cnt = 1
+            f.write(first_text)
+
+    if cnt < 10:
+        cnt = '0' + str(cnt)
+    f.write(str(cnt))
+    f.close()
+
+text_encode(raw_text)
+
+with open("encode.txt") as f:
+    encode_text = f.read()
+print(f'encode text: {encode_text}')
+
+# 復元を行う関数：text_decode
+def text_decode(encode_text):
+    f = open('decode.txt', 'w')
+    idx_list = []
+    for i in range(len(encode_text)):
+        if i % 3 == 0:
+            idx_list.append(int(encode_text[i]))
+        elif i % 3 == 1:
+            idx_list.append(int(encode_text[i:i+2]))
+
+    for j in range(len(idx_list)):
+        if j % 2 == 0:
+            f.write(str(idx_list[j])*idx_list[j+1])
+
+    f.close()
+
+text_decode(encode_text)
+
+with open("decode.txt") as f:
+    decode_text = f.read()
+print(f'decode text: {decode_text}')
+
+print(raw_text == decode_text)
